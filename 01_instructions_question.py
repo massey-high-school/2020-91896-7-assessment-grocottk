3ec17558f6326920f3fc154e438eec930940a383
Kahlil <51727305+grocottk@users.noreply.github.com>
2020-06-05 21:54:32 +1200

    Initial Component 1 Build
    This is the initial Commit of my Component 1, which aims to ask the user whether they have used the program before, and print questions appropriately. This build simply outlines the capability of the Component, and defines an Instructions Function.

---
+++ 01_instructions_question.py
@@ -0,0 +1,11 @@
+
+# Geometry Assessment Component 1
+# This Component asks the user whether they have used the program before. If they have not,
+# ... instructions will be printed, while if they have, instructions will not be printed.
+
+# Functions:
+
+
+def instructions():
+
+    print('''Welcome to the Geometry Calculator''')
