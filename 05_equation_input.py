a0a5e899b00ca8d19d0009a51fbd78d3726a5121
Kahlil <51727305+grocottk@users.noreply.github.com>
2020-06-22 12:31:29 +1200

    Component 5 with Base Functions Added
    This build of Component 5 simply asks the user for their desired shape, however stops before further input may be used.

--- 05_equation_input.py
+++ 05_equation_input.py
@@ -1,3 +1,83 @@
 
 # Geometry Assessment Component 5
 # This Component asks the user for the type of their shape, and a specific measurement associated with that shape.
+
+# *** Functions: ***
+
+# Shape List Formatter (Formats 'valid_shapes' list in order to improve usability)
+# [Code adapted from '00_sandpit.py' created by Ms. Gottschalk.
+# ... This code has been adapted through variable name changes and comments that describe the code's purpose]
+
+
+def shape_list_formatting():
+
+    # Defines 'shape_list' variable
+
+    shape_list = ""
+
+    # Adds every item in the 'valid_shapes' list into a sentence
+
+    for item in valid_shapes:
+
+        # Adds spaces between each list item
+
+        shape_list += ", "
+
+        # Adds all items into the shape_list variable
+
+        shape_list += item
+
+    return shape_list
+
+# Shape Checker (Checks to see which shape the user has chosen from the options given)
+# ... [This program is a customised version of the '02_yes_no_checker.py' by Ms. Gottschalk which checks shape types]
+
+# Defines list of valid shapes to check against
+
+valid_shapes = ["square", "circle", "rectangle", "triangle"]
+
+
+def shape_choice(question):
+
+    # Begin Loop
+
+    valid = False
+    while not valid:
+
+        # Defines response variable (as well as converting input into all lowercase letters)
+
+        response = input(question).lower()
+
+        # Checks input against the valid shapes in the 'valid_shapes' list
+
+        for item in valid_shapes:
+
+            # If the response is in the valid shapes list, return the response
+
+            if response == item:
+
+                return response
+
+            # If a single letter us entered and matches the first letter of any of the shape names,
+            # ... return the shape name.
+
+            elif response == item[0]:
+
+                return item
+
+        # Otherwise, print an error message
+
+        print("Please enter a valid shape from{}".format(shape_list_formatting()))
+
+# *** Main Routine: ***
+
+# * Receiving Shape Type: * [Inspired by subheadings from '11_post_usability_testing_outcome.py' by Kahlil Grocott]
+
+# Prints the formatted, editable shape list to gain input
+
+shape_type = shape_choice("Please enter a shape from the following{} ".format(shape_list_formatting()))
+
+# Prints the user's shape choice for testing
+
+print('''
+You chose the {}'''.format(shape_type))
